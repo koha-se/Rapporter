6cbb7db4cc5c0d66238d84f4076ea1d68f77d874
olssonnic <olsson.nic@gmail.com>
2026-09-04 10:13:37 +0200

    Lägg till 'WHERE datecancelled IS NULL'. Ta bort hårdkodat värde

--- Cirkulation - Stockholms universitetsbibliotek/Material under transport.sql
+++ Cirkulation - Stockholms universitetsbibliotek/Material under transport.sql
@@ -4,6 +4,6 @@ LEFT JOIN items i ON (bt.itemnumber = i.itemnumber)
 LEFT JOIN biblio b ON (i.biblionumber = b.biblionumber)
 WHERE bt.datesent <= DATE_SUB(CURDATE(),interval 2 week)
 AND bt.datearrived IS NULL
-AND i.itemlost NOT LIKE '4'
+AND datecancelled IS NULL
 AND i.homebranch = <<HEMENHET|branches>>
 GROUP BY bt.frombranch, bt.tobranch, i.homebranch, i.itemcallnumber, i.barcode, b.title, b.author, bt.datesent
